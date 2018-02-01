3630e32e968a08031e0d7d1ede1bd97f2351822b
kessigan <kessigan.subramanian@gmail.com>
2018-02-02 00:40:00 +0200

    Objective4
    uploaded data to database using mysql workbench as the post request was
    not fixed

--- create.js
+++ create.js
@@ -1,9 +1,9 @@
 var mysql      = require('mysql');
 var con = mysql.createConnection({
-  host     : 'us-cdbr-iron-east-05.cleardb.net',
-  user     : 'bdfb4b0da12a21',
-  password : '6dbffacf',
-  database : 'heroku_92916039ae377d2'
+  host     : 'sql11.freemysqlhosting.net',
+  user     : 'sql11219062',
+  password : 'jYEteHwenl',
+  database : 'sql11219062'
 });
 
 
